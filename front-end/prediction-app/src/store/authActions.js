ef41fd6a3b9d6c56b9f7fc46b3b0d7ea6d64c87b
jian.ouyang1 <jian.ouyang1@yintech.cn>
2023-10-13 14:28:02 +1300

    fix predict error

--- front-end/prediction-app/src/store/authActions.js
+++ front-end/prediction-app/src/store/authActions.js
@@ -71,7 +71,7 @@ export const authCheckTimeout = expirationTime => {
 export const authLogin = (username, password) => {
     return dispatch => {
         dispatch(authStart());
-        if(username == 'admin' && password =='admin') {
+        if(username == 'admin' && password == 'admin') {
             const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
             const expirationDate = new Date(new Date().getTime() + SESSION_DURATION );
             localStorage.setItem('token', token);
